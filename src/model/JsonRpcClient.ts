ef9f9c6a238f3da9d2c9b304d2182b9af1b68167
Ivan Ho <ihkh2@cam.ac.uk>
2025-05-29 14:30:29 +0100

    electron + r2d2 server

---
+++ src/model/JsonRpcClient.ts
@@ -0,0 +1,9 @@
+import * as rpc from "vscode-jsonrpc/node";
+import { R2D2ServerInterface } from "./R2D2ServerInterface";
+
+export class JsonRpcClient {
+  connection: rpc.MessageConnection;
+
+  constructor() {
+  }
+}
---
+++ src/model/R2D2ServerInterface.d.ts
@@ -0,0 +1,29 @@
+
+
+export namespace R2D2ServerInterface {
+  export interface FileLineCol {
+    filename: string;
+    line: number;
+    column: number;
+  }
+
+  export enum TraceDirection {
+    Backward = "back",
+    Forward = "fwd",
+  }
+
+  export interface TraceRequest {
+    source: FileLineCol;
+    traceDirection: TraceDirection;
+    maxDepth: number;
+  }
+
+  export interface TraceResponse {
+    locations: FileLineCol[];
+  }
+
+  export type Methods = {
+    "r2d2/load"(params: string): "success" | "failure";
+    "r2d2/trace"(params: TraceRequest): TraceResponse;
+  }
+}
